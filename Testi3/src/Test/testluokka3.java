29a0582091730bf3535bfff83a6d33b6055a6b6f
Koti <Koti@Koti-PC>
2017-02-25 15:10:24 +0200

    Kolmaskommitti

---
+++ Testi3/src/Test/testluokka3.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class testluokka3 {
+
+	public testluokka3() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
