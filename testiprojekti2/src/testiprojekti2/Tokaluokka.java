3744ed04042bec80d69a09971c5ce19fea0abf04
Koti <Koti@Koti-PC>
2017-02-25 15:07:49 +0200

    EkaTokaluokka2

---
+++ testiprojekti2/src/testiprojekti2/Tokaluokka.java
@@ -0,0 +1,14 @@
+package testiprojekti2;
+
+public class Tokaluokka {
+
+	public Tokaluokka() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
