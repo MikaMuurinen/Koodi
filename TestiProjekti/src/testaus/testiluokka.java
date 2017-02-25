aa8fb83fea2a713d6d33d7d955379d25225a9e19
Koti <Koti@Koti-PC>
2017-02-25 15:03:16 +0200

    eka committi

---
+++ TestiProjekti/src/testaus/testiluokka.java
@@ -0,0 +1,14 @@
+package testaus;
+
+public class testiluokka {
+
+	public testiluokka() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Just testing");
+	}
+
+}
